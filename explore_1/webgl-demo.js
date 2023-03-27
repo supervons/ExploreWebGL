2908dcd0aaa5f338b331cd314e0668e3b962d0bd
supervons <supervons@sina.com>
2023-03-27 10:40:44 +0800

    Use u_time, but it's not work like : https://thebookofshaders.com/03/ demo.

--- explore_1/webgl-demo.js
+++ explore_1/webgl-demo.js
@@ -16,9 +16,11 @@ function main() {
     #ifdef GL_ES
       precision mediump float;
     #endif
+    
+    uniform float u_time;
 
     vec4 red(){
-      return vec4(0.0, 1.0, 1.0, 1.0); //This num calculated as 1/255 * rgb[num] 
+      return vec4(0.0, 1.0, abs(sin(u_time)), 1.0); //This num calculated as 1/255 * rgb[num] 
     }
         
     void main() {
